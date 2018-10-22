78abd14253f80c32b5a4ab27edbf1acc7701e2e6
LawrenceHong <2395935332@qq.com>
2018-10-22 01:41:24 -0500

    fomat code

--- services/util/sendText.js
+++ services/util/sendText.js
@@ -1,7 +1,7 @@
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
 // Set region
-AWS.config.update({region: 'REGION'});
+AWS.config.update({region: 'us-east-2'});
 
 exports.text = (params) => {
     var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
